45b58eb5bb0a353909f10f18b67e0f24c73233cd
Marian ANDRE <mandre@student.42.fr>
2017-03-29 14:16:03 +0200

    [UPD] V3 - first version

--- src/Constants.php
+++ src/Constants.php
@@ -2,30 +2,28 @@
 
 namespace RecastAI;
 
-/**
- * Class Constants
- * @package RecastAI
- */
 class Constants
 {
-  const API_ENDPOINT = 'https://api.recast.ai/v2/request';
-  const API_ENDPOINT_CONVERSATION = 'https://api.recast.ai/v2/converse';
+  const REQUEST_ENDPOINT      = "https://api-development.recast.ai/v2/request";
+  const CONVERSE_ENDPOINT     = "https://api-development.recast.ai/v2/converse";
+  const CONVERSATION_ENDPOINT = "https://api-development.recast.ai/connect/v1/messages";
+  const MESSAGE_ENDPOINT      = "https://api-development.recast.ai/connect/v1/conversations/:conversation_id/messages";
 
-  const ACT_ASSERT = 'assert';
-  const ACT_COMMAND = 'command';
-  const ACT_WH_QUERY = 'wh-query';
-  const ACT_YN_QUERY = 'yn-query';
+  const ACT_ASSERT    = "assert";
+  const ACT_COMMAND   = "command";
+  const ACT_WH_QUERY  = "wh-query";
+  const ACT_YN_QUERY  = "yn-query";
 
-  const TYPE_ABBREVIATION = 'abbr:';
-  const TYPE_ENTITY = 'enty:';
-  const TYPE_DESCRIPTION = 'desc:';
-  const TYPE_HUMAN = 'hum:';
-  const TYPE_LOCATION = 'loc:';
-  const TYPE_NUMBER = 'num:';
+  const TYPE_ABBREVIATION = "abbr:";
+  const TYPE_ENTITY       = "enty:";
+  const TYPE_DESCRIPTION  = "desc:";
+  const TYPE_HUMAN        = "hum:";
+  const TYPE_LOCATION     = "loc:";
+  const TYPE_NUMBER       = "num:";
 
-  const SENTIMENT_POSITIVE = 'positive';
-  const SENTIMENT_NEUTRAL = 'neutral';
-  const SENTIMENT_NEGATIVE = 'negative';
-  const SENTIMENT_VPOSITIVE = 'vpositive';
-  const SENTIMENT_VNEGATIVE = 'vnegative';
+  const SENTIMENT_VERY_POSITIVE = "vpositive";
+  const SENTIMENT_POSITIVE      = "positive";
+  const SENTIMENT_NEUTRAL       = "neutral";
+  const SENTIMENT_NEGATIVE      = "negative";
+  const SENTIMENT_VERY_NEGATIVE = "vnegative";
 }
