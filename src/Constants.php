715c3bae5fd46bf7a4fc94f50d37bda9b5ed489e
Marian ANDRE <mandre@student.42.fr>
2017-03-30 16:07:26 +0200

    [UPD] Add route

--- src/Constants.php
+++ src/Constants.php
@@ -4,10 +4,10 @@ namespace RecastAI;
 
 class Constants
 {
-  const REQUEST_ENDPOINT      = "https://api-staging.recast.ai/v2/request";
-  const CONVERSE_ENDPOINT     = "https://api-staging.recast.ai/v2/converse";
-  const CONVERSATION_ENDPOINT = "https://api-staging.recast.ai/connect/v1/messages";
-  const MESSAGE_ENDPOINT      = "https://api-staging.recast.ai/connect/v1/conversations/:conversation_id/messages";
+  const REQUEST_ENDPOINT      = "https://api.recast.ai/v2/request";
+  const CONVERSE_ENDPOINT     = "https://api.recast.ai/v2/converse";
+  const CONVERSATION_ENDPOINT = "https://api.recast.ai/connect/v1/messages";
+  const MESSAGE_ENDPOINT      = "https://api.recast.ai/connect/v1/conversations/:conversation_id/messages";
 
   const ACT_ASSERT    = "assert";
   const ACT_COMMAND   = "command";
